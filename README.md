3977e45eeff15ec1e3b87163308efebbee3e8556
chrisrosana <chrisrosana@yahoo.com>
2019-11-07 16:34:55 -0800

    added files

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# interpreter-java
+# CSC 413 - Interpreter
+
+## File Structure
+```css
+interpreter-java
+./
+├── factorial.x
+├── factorial.x.cod
+├── fib.x
+├── fib.x.cod
+├── LICENSE
+├── README.md
+├── theinterpreter.pdf
+├── interpreter/
+│   ├── bytecode/
+│   │   ├── AddressLabel.java
+│   │   ├── ArgsCode.java
+│   │   ├── BopCode.java
+│   │   ├── ByteCode.java
+│   │   ├── CallCode.java
+│   │   ├── DumpCode.java
+│   │   ├── FalseBranchCode.java
+│   │   ├── GotoCode.java
+│   │   ├── HaltCode.java
+│   │   ├── LabelCode.java
+│   │   ├── LitCode.java
+│   │   ├── LoadCode.java
+│   │   ├── PopCode.java
+│   │   ├── ReadCode.java
+│   │   ├── ReturnCode.java
+│   │   ├── StoreCode.java
+│   │   └── WriteCode.java
+│   ├── ByteCodeLoader.java
+│   ├── CodeTable.java
+│   ├── Interpreter.java
+│   ├── Program.java
+│   ├── RunTimeStack.java
+│   └── VirtualMachine.java
+└── documentation/
+    └── documentation.pdf
+```
+
+### Java Doc comments written in ByteCode classes and RunTimeStack class are taken from theinterpreter.pdf
+### Credit goes to the author of theinterpreter.pdf
---
+++ interpreter/Program.java
@@ -0,0 +1,56 @@
+package interpreter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import interpreter.bytecode.AddressLabel;
+import interpreter.bytecode.ByteCode;
+import interpreter.bytecode.LabelCode;
+
+public class Program {
+
+    private ArrayList<ByteCode> program;
+
+    public Program() {
+        program = new ArrayList<>();
+    }
+
+    // Courtesy of Jonathan Julian for giving me the idea of creating a separate constructor
+    // I initially had an addByteCode(ByteCode newByteCode) method but decided to go with the constructor for efficiency
+    public Program(ArrayList<ByteCode> loadedByteCodes) {
+        program = loadedByteCodes;
+    }
+
+    protected ByteCode getCode(int pc) {
+        return this.program.get(pc);
+    }
+
+    public int getSize() {
+        return this.program.size();
+    }
+
+    /**
+     * This function should go through the program and resolve all addresses.
+     * Currently all labels look like LABEL <<num>>>, these need to be converted into
+     * correct addresses so the VirtualMachine knows what to set the Program Counter(PC)
+     * HINT: make note what type of data-stucture bytecodes are stored in.
+     */
+    public void resolveAddrs() {
+        HashMap<String, Integer> addresses = new HashMap<>();
+
+        // Store addresses of labels in HashMap
+        for (int i = 0; i < program.size(); i++) {
+            if (program.get(i) instanceof LabelCode) {
+                addresses.put(((LabelCode) program.get(i)).getLabel(), i);
+            }
+        }
+
+        for (ByteCode bc : program) {
+            if (bc instanceof AddressLabel) {
+                // Matches the label in bc with the label in the hashmap and sets the same address
+                // bc.setAddress(addresses.get(bc.getLabel()));
+                ((AddressLabel) bc).setAddress(addresses.get(((AddressLabel) bc).getLabel()));
+            }
+        }
+    }
+}
